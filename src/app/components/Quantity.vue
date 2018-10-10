37534aca13ad903fe674ccf2df8523255d59d8a5
frank.gairal <gairal@ekino.com>
2018-10-10 11:20:41 -0700

    fix(style): upgrade style

--- src/app/components/Quantity.vue
+++ src/app/components/Quantity.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     v-model="value"
-    class="border w-full h-10 text-3xl p-4 mb-4"
+    class="border w-full h-full h-10 text-3xl p-4 mb-4 flex-1 text-center"
     type="number"
     placeholder="42"
     @keypress="isNumber" />
